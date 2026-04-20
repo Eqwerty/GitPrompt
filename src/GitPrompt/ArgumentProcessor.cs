a053e33fed10c6cb87777e3d6c6e372ed1b33a05
Eduardo Quintana <eduardo.quintana@supergroup.com>
2026-04-20 19:50:22 +0200

    Handle unknown command

--- src/GitPrompt/ArgumentProcessor.cs
+++ src/GitPrompt/ArgumentProcessor.cs
@@ -13,7 +13,8 @@ internal static class ArgumentProcessor
 
         if (!CommandRegistry.TryGetCommandByVerb(arguments[0], out var command))
         {
-            return;
+            Console.Error.WriteLine($"gitprompt: '{arguments[0]}' is not a gitprompt command. See gitprompt --help.");
+            Environment.Exit(1);
         }
 
         command.Execute(arguments);
