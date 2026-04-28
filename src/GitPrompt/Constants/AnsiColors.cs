aa837cc208e93a142131c30c45c58b6cdff535b0
Eduardo Quintana <eduardo.quintana@supergroup.com>
2026-04-28 16:30:00 +0200

    Update colors

--- src/GitPrompt/Constants/AnsiColors.cs
+++ src/GitPrompt/Constants/AnsiColors.cs
@@ -5,12 +5,12 @@ internal static class AnsiColors
     private const string Escape = "\e";
 
     internal const string Green = $"{Escape}[0;32m";
-    internal const string BoldMagenta = $"{Escape}[1;35m";
+    internal const string Magenta = $"{Escape}[38;2;203;6;178m";
     internal const string Orange = $"{Escape}[38;5;172m";
-    internal const string BoldCyan = $"{Escape}[1;36m";
     internal const string Red = $"{Escape}[0;31m";
     internal const string BoldRed = $"{Escape}[1;31m";
     internal const string LightGray = $"{Escape}[0;37m";
-    internal const string Yellow = $"{Escape}[0;33m";
+    internal const string Yellow = $"{Escape}[38;2;255;160;2m";
+    internal const string Blue = $"{Escape}[38;2;72;168;205m";
     internal const string Reset = $"{Escape}[0m";
 }
--- src/GitPrompt/Constants/PromptColors.cs
+++ src/GitPrompt/Constants/PromptColors.cs
@@ -8,19 +8,19 @@ internal static class PromptColors
     private static string PromptColor(string ansiColor) => $"{ReadLineStart}{ansiColor}{ReadLineEnd}";
 
     internal static readonly string ColorUser = PromptColor(AnsiColors.Green);
-    internal static readonly string ColorHost = PromptColor(AnsiColors.BoldMagenta);
+    internal static readonly string ColorHost = PromptColor(AnsiColors.Magenta);
     internal static readonly string ColorPath = PromptColor(AnsiColors.Orange);
-    internal static readonly string ColorBranch = PromptColor(AnsiColors.BoldCyan);
-    internal static readonly string ColorBranchNoUpstream = PromptColor(AnsiColors.BoldCyan);
-    internal static readonly string ColorAhead = PromptColor(AnsiColors.BoldCyan);
-    internal static readonly string ColorBehind = PromptColor(AnsiColors.BoldCyan);
+    internal static readonly string ColorBranch = PromptColor(AnsiColors.Blue);
+    internal static readonly string ColorBranchNoUpstream = PromptColor(AnsiColors.Blue);
+    internal static readonly string ColorAhead = PromptColor(AnsiColors.Blue);
+    internal static readonly string ColorBehind = PromptColor(AnsiColors.Blue);
     internal static readonly string ColorStaged = PromptColor(AnsiColors.Green);
     internal static readonly string ColorUnstaged = PromptColor(AnsiColors.Red);
     internal static readonly string ColorUntracked = PromptColor(AnsiColors.Red);
-    internal static readonly string ColorStash = PromptColor(AnsiColors.BoldMagenta);
+    internal static readonly string ColorStash = PromptColor(AnsiColors.Magenta);
     internal static readonly string ColorConflict = PromptColor(AnsiColors.BoldRed);
     internal static readonly string ColorMissingPath = PromptColor(AnsiColors.BoldRed);
-    internal static readonly string ColorCommandDuration = PromptColor(AnsiColors.BoldMagenta);
+    internal static readonly string ColorCommandDuration = PromptColor(AnsiColors.Magenta);
     internal static readonly string ColorTimeout = PromptColor(AnsiColors.Yellow);
     internal static readonly string ColorPromptSymbol = PromptColor(AnsiColors.LightGray);
     internal static readonly string ColorReset = PromptColor(AnsiColors.Reset);
