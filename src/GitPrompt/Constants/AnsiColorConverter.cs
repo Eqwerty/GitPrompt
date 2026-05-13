8a356788c21bca9cb167938e2e70153d0e53cfd6
Eduardo Quintana <eduardo.quintana@supergroup.com>
2026-05-13 21:30:00 +0200

    Clean up AnsiColorConverter

--- src/GitPrompt/Constants/AnsiColorConverter.cs
+++ src/GitPrompt/Constants/AnsiColorConverter.cs
@@ -16,67 +16,54 @@ internal static class AnsiColorConverter
 
     private static string HexToAnsi(string hexColor)
     {
-        if (hexColor.Length is not 7)
+        if (hexColor.Length is not 7 || hexColor.Skip(1).Any(@char => !IsHexDigit(@char)))
         {
             throw new ArgumentException($"Invalid hex color: '{hexColor}'. Expected format: #RRGGBB.", nameof(hexColor));
         }
 
-        for (var i = 1; i < 7; i++)
-        {
-            if (!IsHexDigit(hexColor[i]))
-            {
-                throw new ArgumentException($"Invalid hex color: '{hexColor}'. Expected format: #RRGGBB.", nameof(hexColor));
-            }
-        }
-
-        var r = Convert.ToInt32(hexColor[1..3], 16);
-        var g = Convert.ToInt32(hexColor[3..5], 16);
-        var b = Convert.ToInt32(hexColor[5..7], 16);
+        var r = Convert.ToInt32(hexColor[1..3], fromBase: 16);
+        var g = Convert.ToInt32(hexColor[3..5], fromBase: 16);
+        var b = Convert.ToInt32(hexColor[5..7], fromBase: 16);
 
         return $"\e[38;2;{r};{g};{b}m";
     }
 
-    private static string NormalizeAnsi(string input)
+    private static string NormalizeAnsi(string code)
     {
-        var code = input.AsSpan();
+        if (code.Length is 0)
+        {
+            throw new ArgumentException($"Invalid ANSI color code: '{code}'.", nameof(code));
+        }
 
-        if (code.Length > 0 && code[0] == '\e')
+        if (code[0] is '\e')
         {
             code = code[1..];
         }
-        else if (code.Length > 1 && code[0] == '\\' && code[1] == 'e')
+        else if (code.Length > 1 && code[0] is '\\' && code[1] is 'e')
         {
             code = code[2..];
         }
 
-        if (code.Length > 0 && code[0] == '[')
+        if (code[0] is '[')
         {
             code = code[1..];
         }
 
-        if (code.Length > 0 && code[^1] == 'm')
+        if (code[^1] is 'm')
         {
             code = code[..^1];
         }
 
-        if (code.Length == 0)
+        if (code.Any(@char => !char.IsAsciiDigit(@char) && @char is not ';'))
         {
-            throw new ArgumentException($"Invalid ANSI color code: '{input}'.", nameof(input));
-        }
-
-        foreach (var c in code)
-        {
-            if (!char.IsAsciiDigit(c) && c != ';')
-            {
-                throw new ArgumentException($"Invalid ANSI color code: '{input}'. Expected numeric codes like '32', '1;33', etc.", nameof(input));
-            }
+            throw new ArgumentException($"Invalid ANSI color code: '{code}'. Expected numeric codes like '32', '1;33', etc.", nameof(code));
         }
 
         return $"\e[{code}m";
     }
 
-    private static bool IsHexDigit(char c)
+    private static bool IsHexDigit(char @char)
     {
-        return c is >= '0' and <= '9' or >= 'a' and <= 'f' or >= 'A' and <= 'F';
+        return @char is >= '0' and <= '9' or >= 'a' and <= 'f' or >= 'A' and <= 'F';
     }
 }
