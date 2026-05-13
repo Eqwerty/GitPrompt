3c055531507d711fc754d8897a9e83b5cddfb637
Eduardo Quintana <eduardo.quintana@supergroup.com>
2026-05-13 23:00:03 +0200

    Update windows prompt symbol

--- src/GitPrompt/Constants/PromptSymbols.cs
+++ src/GitPrompt/Constants/PromptSymbols.cs
@@ -2,7 +2,7 @@ namespace GitPrompt.Constants;
 
 internal static class PromptSymbols
 {
-    internal const string Windows = ">";
+    internal const string Windows = "❯";
     internal const string UnixRoot = "#";
     internal const string Unix = "$";
 }
